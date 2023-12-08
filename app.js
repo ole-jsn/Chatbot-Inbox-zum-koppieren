6640c9308f35edfa04f66882c874ce03bdcb087c
ole-jsn <142682188+ole-jsn@users.noreply.github.com>
2023-12-08 17:03:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 7892;
-let receivedDataArray = []; // Array, um empfangene Daten zu speichern
+const port = 3000; // Wähle einen beliebigen Port, den du verwenden möchtest
 
+// Middleware, um JSON-Body aus Anfragen zu verarbeiten
 app.use(express.json());
 
-app.get('/webhook', (req, res) => {
+// POST-Endpunkt für den Webhook
+app.post('/webhook', (req, res) => {
+  // Daten vom Webhook erhalten
   const receivedData = req.body;
-  
-  // Die empfangene Nachricht als weitere ID zum Array hinzufügen
-  receivedDataArray.push(receivedData);
   
   // Hier kannst du mit den empfangenen Daten arbeiten
   console.log('Empfangene Daten:', receivedData);
   
+  // Gib eine Antwort an den Absender des Webhooks zurück
   res.status(200).send('Webhook empfangen');
 });
 
+// Server starten
 app.listen(port, () => {
-  console.log(`Server läuft auf http://localhost:7892/`);
+  console.log(`Server läuft auf http://localhost:${port}`);
 });
 
+// Erhalte die Referenz auf das HTML-Element mit der ID 'receivedDataCell'
+const receivedDataCell = document.getElementById('receivedDataCell');
+
+// Setze den Inhalt der Zelle auf den Wert von receivedData
+receivedDataCell.textContent = receivedData;
